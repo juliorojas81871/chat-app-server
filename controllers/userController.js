55770eeb563c193521cd96fe887b90102722b09b
Julio Rojas <juliorojas81871@gmail.com>
2022-10-03 18:17:10 -0400

    created chat-app-server for aws

---
+++ controllers/userController.js
@@ -0,0 +1,84 @@
+const User = require("../models/userModels");
+const bcrypt = require("bcrypt");
+
+module.exports.register = async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
+    const usernameCheck = await User.findOne({ username });
+    const emailCheck = await User.findOne({ email });
+
+    if (username && emailCheck) {
+      return res.json({ msg: "Username & Email already used", status: false });
+    } else if (usernameCheck) {
+      return res.json({ msg: "Username already used", status: false });
+    } else if (emailCheck) {
+      return res.json({ msg: "Email already used", status: false });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      email,
+      username,
+      password: hashedPassword,
+    });
+    delete user.password;
+    return res.json({ status: true, user });
+  } catch (ex) {
+    next(ex);
+  }
+};
+
+module.exports.login = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.json({ msg: "Incorrect Username or Password", status: false });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.json({ msg: "Incorrect Username or Password", status: false });
+    }
+    delete user.password;
+    return res.json({ status: true, user });
+  } catch (ex) {
+    next(ex);
+  }
+};
+
+module.exports.setAvatar = async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    const avatarImage = req.body.image;
+    const userData = await User.findByIdAndUpdate(
+      userId,
+      {
+        isAvatarImageSet: true,
+        avatarImage,
+      },
+      { new: true }
+    );
+    return res.json({
+      isSet: userData.isAvatarImageSet,
+      image: userData.avatarImage,
+    });
+  } catch (ex) {
+    next(ex);
+  }
+};
+
+module.exports.getAllUsers = async (req, res, next) => {
+  try {
+    // $ne selects the documents where the value of the
+    // field is not equal to the specified value
+    const users = await User.find({ _id: { $ne: req.params.id } }).select([
+      "email",
+      "username",
+      "avatarImage",
+      "_id",
+    ]);
+    return res.json(users);
+  } catch (ex) {
+    next(ex);
+  }
+};
